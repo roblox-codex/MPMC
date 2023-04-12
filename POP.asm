20f5f53b499b251982b8e01fb632e1d24059968a
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-04-13 02:47:55 +0530

    Add files via upload

---
+++ POP.asm
@@ -0,0 +1,13 @@
+ORG 100h 
+MOV AX, 1C2Bh          
+PUSH AX             
+                    
+MOV BX, 302Ah       
+PUSH BX             
+                    
+POP CX              
+                    
+POP [12H]           
+                    
+RET                
+         
