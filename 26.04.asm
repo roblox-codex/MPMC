0efb320ddf238aed0e52b4efa66c8b0e072235ea
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-04-28 01:13:12 +0530

    26.04.asm

--- 26.04.asm
+++ 26.04.asm
@@ -54,3 +54,62 @@ LBL2:
 
 MAIN    ENDP
         END     MAIN
+        
+        
+        
+        
+        
+ ;Q1 
+ ORG 100H
+ NUMBER DW 40 DUP(0)
+ MOV BX, OFFSET NUMBER
+ MOV CX, 40
+ 
+ NEXT:
+ MOV AL, [BX]
+ INC AL
+ MOV [BX], AL
+ ADD BX, 1
+ LOOP NEXT
+ RET
+ 
+ ;Q2
+ ORG 100H
+ ARRAY DB 10 DUP(0)
+ ARRAY_SIZE EQU 10
+ MOV CX, 10
+ MOV BX, OFFSET ARRAY
+ MOV AH, 16D
+ MOV DL, 0H
+ MOV DH, 10D
+ NEXT : MOV AL, [BX]
+ MOV AL, AH
+ INC AH
+ MOV [BX], AL
+ ADD DL, AL
+ LOOOP NEXT:
+ MOV AX, 0000H
+ MOV AL, DL
+ DIV DH
+ HLT RET
+
+;Q3
+org 100h
+array DW 15 dup(0) 
+mov cx, 15 
+mov si, offset array 
+mov [si],00D
+add si,2 
+mov ax,00D
+mov bx,01D 
+loop_start: 
+ mov dx,ax
+ add ax,bx
+ mov [si], ax 
+ mov bx,dx 
+ add si, 2 
+ 
+ loop loop_start 
+ 
+ hlt
+ ret
