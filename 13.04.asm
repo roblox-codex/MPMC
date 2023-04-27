90a1bea30116cae81da5d47bc136e3c754b0cfcc
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-04-28 00:23:24 +0530

    13.04.asm

--- 13.04.asm
+++ 13.04.asm
@@ -87,3 +87,70 @@ MOV BX, 1152H  ;Sets BX to 0022H
 SUB AX, BX     ;Compute AX-BX
 DAS  
 RET
+
+;MUL
+ORG 100h
+.CODE
+MOV AL, 35
+MOV BH, 15
+MUL BH
+RET
+
+;IMUL
+ORG 100h
+.MODEL SMALL
+.CODE
+MOV AL, 2AH       ;Load 1st operand to AX
+MOV BX, -26CH     ;Load 2nd operand in BX
+IMUL BX
+RET
+
+;DIV
+MOV AX, 100    ; Load AX with the dividend
+MOV BX, 10     ; Load BX with the divisor
+DIV BX         ; Divide AX by BX, quotient in AL and remainder in AH
+
+;AND 
+ORG 100h
+AND AX,0008h
+
+;OR
+OR AX, 0098H
+
+;NOT
+MOV AX, 0x0F   ; Move the hexadecimal value 0Fh into the AX register
+NOT AX         ; Perform a bitwise NOT on the contents of the AX register
+
+;XOR
+XOR AX, 0098H
+
+;TEST
+TEST AX, BX
+TEST [0500], 06H
+TEST [BX] [DI], CX
+
+;SHL/SAL
+SHL AX, 1
+
+;SHR
+SHR AX, 1
+
+;JUMP
+ORG 100h   ; Start of program
+
+    ; Do some work here...
+    MOV AX, 0 ; Move the value 0 into the AX register
+    CMP AX, 0 ; Compare the value in AX to zero
+    JNE not_zero ; Jump to not_zero if AX is not equal to zero
+    ; If we get here, AX must be zero
+    ; Do some more work...
+    JMP end ; Jump to the end of the program
+not_zero:
+    ; If we get here, AX is not equal to zero
+    ; Do some different work...
+end:
+    ; End of program
+    MOV AH, 4Ch ; Set the exit code
+    INT 21h    ; Exit the program
+    
+ 
