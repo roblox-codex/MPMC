df3ac9e1e47a008b817f5f62025bfb3692611452
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-05-03 12:18:09 +0530

    03.05.asm

--- 03.05.asm
+++ 03.05.asm
@@ -24,3 +24,25 @@ L2: INC BX;
        LOOP L1;
        MOV [0FFFH], AL
        HLT
+
+;ANS: 2
+ORG 100H
+       MOV AX, 6000H;
+       MOV DS, AX;
+       MOV AX, 30H;
+       MOV BX, 1000H;
+       MOV CX, 96H;
+ARRAY: MOV [BX], AX;
+       ADD AX, 50D;
+       INC BX;
+       LOOP ARRAY;
+       MOV BX, 1000H;
+       MOV AX, [BX];
+       MOV CX, 1FH;
+L1: CMP AX, [BX+1];
+       JNC L2
+       MOV AX, [BX+1]
+L2: INC BX;
+       LOOP L1;
+       MOV [0FFFH], AX
+       HLT
