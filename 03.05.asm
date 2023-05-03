250c6ebd5ca40ef1cd78a43d0f2f9845c4a4e854
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-05-03 11:29:25 +0530

    03.05.asm

---
+++ 03.05.asm
@@ -0,0 +1,4 @@
+;Q1. A program to find out the largest number from a given unorderd array of 8-bit numbers stored in location starting from a known address.
+;Q2. Repeat for 16-bit numbers.
+;Q3. Write a program to find out the number of even an odd numbers from a given series of 16-bit hexadecimal numbers.
+;Q4. Write a program to findout the number of positive numbers and negative numbers from a given series of signed numbers.
