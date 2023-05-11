05ec44d4e86eda44a724c07e1ee897f874b14889
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-05-11 15:38:07 +0530

    Create 11.05.2023.asm

---
+++ 11.05.2023.asm
@@ -0,0 +1,4 @@
+;Q1 Write a program that finds the number of 1's in a byte.
+;Q2 Write a program to count 1's in words provide the count in BCD.
+;Q3 Decide whether the parity of given number is even or odd if parity is even or odd if parity is even set DL = 0 else DL = 1. The given number may be multibyte number.
+;Q4 Write a program to add 2 multibyte number and store the result as a 3rd number. The numbers are stored in the form of byte list stored with the lowest byte first.
